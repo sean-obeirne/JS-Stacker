ebb40a6f0e1dbb6141446db667d7b3b9cbb7765b
Sean O'Beirne <sean.t.obeirne@gmail.com>
2019-06-17 16:14:46 -0400

    implemented full gameplay functionality

--- stacker.js
+++ stacker.js
@@ -8,11 +8,12 @@ let cWidth = 800;
 let cHeight = 800;
 let sx = (cWidth - (bWidth * squareSize)) / 2; //starting x value for board (from left)
 let sy = 25; //starting y value for board (from top)
-let starts = [2, 4, 1, 0, 3, 2, 3, 0, 1, 2, 1, 2] //positions where left most piece starts from on each line going from bottom to top
+let starts = [2, 4, 1, 0, 3, 2, 3, 0, 1, 2, 1, 3] //positions where left most piece starts from on each line going from top to bottom
 let curY = bHeight - 1;
-let curX = starts[0];
+let curX = starts[curY];
 let curSize = 3; //how many pieces to display on row
 let velocity = 1; //1 if piece is moving right, -1 if left
+let paused = false;
 
 document.addEventListener('DOMContentLoaded', setupCanvas);
 
@@ -60,11 +61,42 @@ function setupCanvas(){
 
 window.setInterval(function(){
     move();
-  }, 500);
+  }, 250);
 
 
-function stop(){
+function stopPiece(){
 
+    //make sure we start with only as many pieces landed on the board
+    pieceCount = 0;
+    for(let i = 0; i < bWidth; i++){
+        if(board[curY][i] === 1){
+            pieceCount++;
+        }
+    }
+    curSize = pieceCount;
+
+    //check for pieces that are off the stack
+    if(curY < bHeight - 1){
+        for(let i = 0; i < bWidth; i++){
+            if(board[curY][i] === 1 && board[curY+1][i] === 0){
+                board[curY][i] = 0;
+                drawSquare('gray', i, curY)
+                curSize--;
+                console.log(curSize);
+            }
+        }
+    }
+
+    curY--;
+    if(curY < 0)
+        endGame('You have won!')
+    curX = starts[curY]
+}
+
+
+function endGame(condition){
+    paused = true;
+    console.log(condition)
 }
 
 
@@ -86,34 +118,36 @@ function drawRow(row){
 
 function move(){
 
-    //wipe current row
-    for(let i = 0; i < bWidth; i++){
-        board[curY][i] = 0;
-    }
+    if(paused === false){
+        if(curSize === 0){
+            endGame('You have lost!');
+        }
 
-    let pieceCount = 0;
-    if(curX < bWidth && curX >= 0)
-        board[curY][curX] = 1;
-    if(bHeight - curY < 6 && curSize > 1 && curX+1 < bWidth && curX+1 >= 0){ //there should be at least 2 pieces on this row
-        board[curY][curX+1] = 1;
-        pieceCount++;
-    }
-    if(bHeight - curY < 3 && curSize > 2 && curX+2 < bWidth && curX+2 >= 0){ //there should be 3 pieces on this row
-        board[curY][curX+2] = 1;
-        pieceCount++;
-    }
+        //wipe current row
+        for(let i = 0; i < bWidth; i++){
+            board[curY][i] = 0;
+        }
 
-    drawRow(curY);
+        let pieceCount = 0;
+        if(curX < bWidth && curX >= 0 && curSize > 0)
+            board[curY][curX] = 1;
+        if(bHeight - curY <= 6 && curSize > 1 && curX+1 < bWidth && curX+1 >= 0) //there should be at least 2 pieces on this row
+            board[curY][curX+1] = 1;
+        if(bHeight - curY <= 3 && curSize > 2 && curX+2 < bWidth && curX+2 >= 0) //there should be 3 pieces on this row
+            board[curY][curX+2] = 1;
 
-    if(curX == bWidth-1)
-        velocity = -1;
-    else if(curX+pieceCount <= 0)
-        velocity = 1;
-    curX += velocity;
+        drawRow(curY);
+
+        if(curX == bWidth-1)
+            velocity = -1;
+        else if(curX+curSize-1 <= 0)
+            velocity = 1;
+        curX += velocity;
+    }
 }
 
 
 function handleKeyPress(key){
-    if (key.keyCode === 32)
-        stop();
+    if (key.keyCode === 32 && paused === false)
+        stopPiece();
 }
